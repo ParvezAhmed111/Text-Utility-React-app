47222ab7c4a270416f395c09ee07bcf6d3fde6ac
Sheikh Parvez Ahmed <parvezahmed.0947@gmail.com>
2021-09-24 22:01:05 +0530

    D8

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
-// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
+  const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enables", "success");
+      document.title = "TextUtils - Dark Mode";
+      // setInterval(() => {
+      //   document.title= "TextUtils is Amazing Mode";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title= "Install TextUtils";
+      // }, 1500);
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enables", "success");
+      document.title = "TextUtils - Light Mode";
+
+    }
+  }
   return (
-    <>  
-      <Navbar title="TextUtils" aboutText="About Us"/>
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyse below"/>
-      </div>
+    <>
+      <Router>
+        {/* <Navbar title="TextUtils" aboutText="About Us"/> */}
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            {/* /users --> Component 1
+                /users/home --> Component 2 */}
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm showAlert={showAlert} heading="Enter the text to analyse below" mode={mode} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
 
 export default App;
+
+
+
 
 
 
